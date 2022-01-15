b031524921dcb582951486ae01e97a6bdf393463
Whose dream is this? <whosedreamisthis@gmail.com>
2022-01-15 11:00:29 -0800

    integrate apis

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const button = document.querySelector("#button");
 
 const audioElement = document.querySelector("#audio");
 // VoiceRSS Javascript SDK
+// prettier-ignore
 const VoiceRSS = {
   speech: function (e) {
     this._validate(e), this._request(e);
@@ -106,10 +107,15 @@ const VoiceRSS = {
   },
 };
 
-function test() {
+//Disable/Enable button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+async function tellJoke(joke) {
+  console.log(joke);
   VoiceRSS.speech({
     key: "2b8e5afb1502446382081bb82ea4146a",
-    src: "Hello, world!",
+    src: joke,
     hl: "en-us",
     v: "Linda",
     r: 0,
@@ -119,4 +125,40 @@ function test() {
   });
 }
 
-test();
+//get jokes from jokes API
+async function getJokes() {
+  let joke = "";
+  const apiUrl =
+    "https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    tellJoke(joke);
+    toggleButton();
+  } catch (error) {
+    console.log("whoops", error);
+  }
+}
+
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
+// function test() {
+//   VoiceRSS.speech({
+//     key: "2b8e5afb1502446382081bb82ea4146a",
+//     src: "Hello, world!",
+//     hl: "en-us",
+//     v: "Linda",
+//     r: 0,
+//     c: "mp3",
+//     f: "44khz_16bit_stereo",
+//     ssml: false,
+//   });
+// }
+
+// test();
